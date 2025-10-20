4d3d530fd754d68913cca0b0067bc140aa2bbd88
Gergő Móricz <mo.geryy@gmail.com>
2025-10-20 02:36:03 +0200

    Revert "fix(nuq): conflict fixes"
    This reverts commit 851f6309e5528ca3b53510bf966fb5940ececea6.

--- apps/api/src/services/worker/nuq.ts
+++ apps/api/src/services/worker/nuq.ts
@@ -926,7 +926,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_owner_concurrency (id, current_concurrency, max_concurrency)
             SELECT owner_id, 0, ${this.queueName.replaceAll(".", "_")}_owner_resolve_max_concurrency(owner_id)
             FROM missing_owners
-            ON CONFLICT (id) DO NOTHING
           ),
           updated AS (
             UPDATE ${this.queueName} q
@@ -1002,7 +1001,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_owner_concurrency (id, current_concurrency, max_concurrency)
             SELECT owner_id, 0, ${this.queueName.replaceAll(".", "_")}_owner_resolve_max_concurrency(owner_id)
             FROM missing_owners
-            ON CONFLICT (id) DO NOTHING
           ),
           distinct_groups AS (
             SELECT DISTINCT group_id
@@ -1021,7 +1019,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_group_concurrency (id, current_concurrency, max_concurrency)
             SELECT group_id, 0, NULL
             FROM missing_groups
-            ON CONFLICT (id) DO NOTHING
           ),
           updated AS (
             UPDATE ${this.queueName} q
@@ -1165,7 +1162,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_owner_concurrency (id, current_concurrency, max_concurrency)
             SELECT owner_id, 0, ${this.queueName.replaceAll(".", "_")}_owner_resolve_max_concurrency(owner_id)
             FROM missing_owners
-            ON CONFLICT (id) DO NOTHING
           ),
           updated AS (
             UPDATE ${this.queueName} q
@@ -1237,7 +1233,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_owner_concurrency (id, current_concurrency, max_concurrency)
             SELECT owner_id, 0, ${this.queueName.replaceAll(".", "_")}_owner_resolve_max_concurrency(owner_id)
             FROM missing_owners
-            ON CONFLICT (id) DO NOTHING
           ),
           distinct_groups AS (
             SELECT DISTINCT group_id
@@ -1256,7 +1251,6 @@ class NuQ<JobData = any, JobReturnValue = any> {
             INSERT INTO ${this.queueName}_group_concurrency (id, current_concurrency, max_concurrency)
             SELECT group_id, 0, NULL
             FROM missing_groups
-            ON CONFLICT (id) DO NOTHING
           ),
           updated AS (
             UPDATE ${this.queueName} q
