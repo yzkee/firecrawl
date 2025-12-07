7e2328ad1ded7797c5a3af1ea568d4d7b534e8e9
rafaelmmiller <150964962+rafaelsideguide@users.noreply.github.com>
2025-12-07 14:57:14 -0300

    chore: bump sdk versions

--- apps/python-sdk/firecrawl/__init__.py
+++ apps/python-sdk/firecrawl/__init__.py
@@ -17,7 +17,7 @@ from .v1 import (
     V1ChangeTrackingOptions,
 )
 
-__version__ = "4.10.2"
+__version__ = "4.10.3"
 
 # Define the logger for the Firecrawl project
 logger: logging.Logger = logging.getLogger("firecrawl")
