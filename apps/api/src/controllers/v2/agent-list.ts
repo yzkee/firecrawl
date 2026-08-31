162e660426f2f1fa2947eceecef1244bc7f35600
Gergő Móricz <mo.geryy@gmail.com>
2026-08-31 23:17:35 +0200

    fix(api/agent/list): fix date parsing again (#4472)

--- apps/api/src/controllers/v2/agent-list.ts
+++ apps/api/src/controllers/v2/agent-list.ts
@@ -102,7 +102,7 @@ export async function agentListController(
           before: (parsedBefore !== undefined
             ? new Date(parsedBefore).toISOString()
             : new Date().toISOString()
-          ).slice(0, -1), // slice Z off the end of the ISO string because clickhouse hates it
+          ).split(".")[0], // slice .nnnZ off the end of the ISO string because clickhouse hates it
         },
         format: "JSONEachRow",
       });
